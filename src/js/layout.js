a1432cef454f289d4949e9d23af38f6885ea582e
chairon01 <chairon00@gmail.com>
2020-02-07 23:05:58 +0000

    update

--- src/js/layout.js
+++ src/js/layout.js
@@ -5,6 +5,7 @@ import ScrollToTop from "./component/scrollToTop";
 import { Home } from "./views/home";
 import { Demo } from "./views/demo";
 import { Single } from "./views/single";
+import { Link } from "react-scroll";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,48 +4,44 @@ import { Card } from "../component/Card";
 import { Modal } from "../component/Modal";
 
 export const Home = () => (
-	<div className="container">
-		<row>
-			<div col-md-4>
-				<div id="carouselExampleFade" className="carousel slide carousel-fade" data-ride="carousel">
-					<div className="carousel-inner">
-						<div className="carousel-item active">
-							<img src="https://picsum.photos/id/237/1500/700" className="d-block " alt="..." />
-							<div className="text-carousel-1">
-								<h1>
-									<b>ERES GAY </b>
-								</h1>
-							</div>
-						</div>
-						<div className="carousel-item">
-							<img src="https://picsum.photos/seed/picsum/1500/700" className="d-block " alt="..." />
-							<div className="text-carousel-1">
-								<h1>
-									<b>SOMOS TU SOLUCIÓN PARA TUS EVENTOS </b>
-								</h1>
-							</div>
-						</div>
-						<div className="carousel-item">
-							<img src="https://picsum.photos/1500/700?grayscale" className="d-block " alt="..." />
-							<div className="text-carousel-1">
-								<h1>
-									<b>CELEBRALO TODO</b>
-								</h1>
-							</div>
-						</div>
+	<React.Fragment>
+		<div id="carouselExampleFade" className="carousel slide carousel-fade" data-ride="carousel">
+			<div className="carousel-inner">
+				<div className="carousel-item active">
+					<img src="https://picsum.photos/id/237/1500/700" className="d-block " alt="..." />
+					<div className="text-carousel-1">
+						<h1>
+							<b>ERES GAY </b>
+						</h1>
 					</div>
-					<a className="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
-						<span className="carousel-control-prev-icon" aria-hidden="true" />
-						<span className="sr-only">Antes</span>
-					</a>
-					<a className="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
-						<span className="carousel-control-next-icon" aria-hidden="true" />
-						<span className="sr-only">Próximo</span>
-					</a>
+				</div>
+				<div className="carousel-item">
+					<img src="https://picsum.photos/seed/picsum/1500/700" className="d-block " alt="..." />
+					<div className="text-carousel-1">
+						<h1>
+							<b>SOMOS TU SOLUCIÓN PARA TUS EVENTOS </b>
+						</h1>
+					</div>
+				</div>
+				<div className="carousel-item">
+					<img src="https://picsum.photos/1500/700?grayscale" className="d-block " alt="..." />
+					<div className="text-carousel-1">
+						<h1>
+							<b>CELEBRALO TODO</b>
+						</h1>
+					</div>
 				</div>
 			</div>
-			<Card />
-			<Modal />
-		</row>
-	</div>
+			<a className="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+				<span className="carousel-control-prev-icon" aria-hidden="true" />
+				<span className="sr-only">Antes</span>
+			</a>
+			<a className="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+				<span className="carousel-control-next-icon" aria-hidden="true" />
+				<span className="sr-only">Próximo</span>
+			</a>
+		</div>
+		<Card />
+		<Modal />
+	</React.Fragment>
 );
